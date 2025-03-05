818a4e8b5be86e03a5f20ccb4edf9e9a2af386c6
elias <alvaroalbarracin02@gmail.com>
2025-03-05 12:48:54 +0100

    adsfa

---
+++ src/js/contacto.js
@@ -0,0 +1,4 @@
+document.querySelector("#boton").addEventListener("click",() => {
+    alert("Gracias por su mensaje, le responderemos lo antes posible");
+    
+});
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import "bootstrap"
+import "@fortawesome/fontawesome-free/js/all.js"
+
+console.log("plantilla de Juan que se lo tiene muy creido");
